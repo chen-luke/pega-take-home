6ea95a30c20627432dc03941d58e3f601feb1831
chen-luke <72319988+chen-luke@users.noreply.github.com>
2025-10-24 18:00:42 -0400

    implemented hover and fixed input menu displaying correctly in shrunken sidenav

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -1,16 +1,26 @@
+import { useState } from 'react';
 import SearchBar from '../atoms/search-bar/SearchBar';
 import AppUtil from './app-util/AppUtil';
 import styles from './navigation.module.css';
 import ScrollablePanel from './scrollable-panel/ScrollablePanel';
 
 function Navigation() {
+  const [isHovering, setIsHovering] = useState(false);
+
+  const hoverStyle = { width: isHovering ? '331px' : '80px' };
+
   return (
-    <nav className={styles.navigation}>
+    <nav
+      className={styles.navigation}
+      style={hoverStyle}
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
+    >
       <div>
         <span>PEGA</span>
         <span>Cosmos</span>
       </div>
-      <SearchBar />
+      <SearchBar isHovering={isHovering} />
       <ScrollablePanel />
       <AppUtil />
     </nav>
--- src/atoms/search-bar/SearchBar.tsx
+++ src/atoms/search-bar/SearchBar.tsx
@@ -1,11 +1,15 @@
 import styles from './search-bar.module.css';
 import { FaSearch } from 'react-icons/fa';
 
-function SearchBar() {
-  return (
+function SearchBar({ isHovering }: { isHovering: boolean }) {
+  return isHovering ? (
     <div className={styles.searchContainer}>
       <FaSearch className={styles.searchIcon} />
       <input placeholder='search' className={styles.searchBar} />
+    </div>
+  ) : (
+    <div className={styles.containerNoHover}>
+      <FaSearch className={styles.searchIcon} />
     </div>
   );
 }
