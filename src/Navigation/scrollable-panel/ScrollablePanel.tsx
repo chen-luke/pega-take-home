c6b99decbf2442de4276e6dd384bf73fc07d8733
chen-luke <72319988+chen-luke@users.noreply.github.com>
2025-10-25 09:44:26 -0400

    implemented scollable panel components

--- src/Navigation/scrollable-panel/ScrollablePanel.tsx
+++ src/Navigation/scrollable-panel/ScrollablePanel.tsx
@@ -2,21 +2,21 @@ import styles from './scrollable-panel.module.css';
 import { menuLinks } from '../../util/dummylinks';
 import ListItem from '../../atoms/list-item/ListItem';
 
-function ScrollAblePanel() {
+function ScrollAblePanel({ isHovering }: { isHovering: boolean }) {
+  const extendedMenuLinks = [
+    ...menuLinks,
+    // ...menuLinks,
+    // ...menuLinks,
+  ];
+
   return (
-    <ul className={styles.scrollableGroup}>
-      {menuLinks.map((link) => (
-        <ListItem
-          key={link.label}
-          link={{
-            label: link.label,
-            icon: link.icon,
-            slug: link.slug,
-            state: link.state,
-          }}
-        />
-      ))}
-    </ul>
+    <div className={styles.scrollableGroup}>
+      <ul>
+        {extendedMenuLinks.map((link) => (
+          <ListItem isHovering={isHovering} key={link.label} link={link} />
+        ))}
+      </ul>
+    </div>
   );
 }
 
