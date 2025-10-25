5f8d1f4f741e94482deb6381eefff521cf69ef02
chen-luke <72319988+chen-luke@users.noreply.github.com>
2025-10-25 09:43:52 -0400

    implemented list item component

--- src/atoms/list-item/ListItem.tsx
+++ src/atoms/list-item/ListItem.tsx
@@ -1,16 +1,39 @@
 import type { LinkType } from '../../util/dummylinks';
+import styles from './list-item.module.css';
+import { Link } from 'react-router-dom';
 
 function ListItem({
-  key,
+  isHovering,
   link,
 }: {
-  key: React.Key | null | undefined;
+  isHovering: boolean;
   link: LinkType;
 }) {
+  const listItemClasses = `${styles.listItem} ${
+    !isHovering ? styles.listItemCollapsed : ''
+  }`;
+
+  const navItemStyle: React.CSSProperties = {
+    textDecoration: 'none',
+    padding: isHovering ? '8px 0px' : '10px 20px',
+    display: 'flex',
+    alignItems: 'center',
+    gap: '10px',
+    color: 'white',
+    background: 'none',
+    border: 'none',
+    cursor: 'pointer',
+    font: 'inherit',
+    width: '100%',
+    justifyContent: isHovering ? 'flex-start' : 'center',
+  };
+
   return (
-    <li key={key}>
-      {link.icon}
-      {link.label}
+    <li key={link.label} className={listItemClasses}>
+      <Link to={link.slug} style={navItemStyle}>
+        {link.icon}
+        {isHovering && link.label}
+      </Link>
     </li>
   );
 }
