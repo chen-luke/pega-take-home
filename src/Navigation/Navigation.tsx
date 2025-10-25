68b42a643a6fdb774a46256900d97bb49a4102f0
chen-luke <72319988+chen-luke@users.noreply.github.com>
2025-10-25 09:46:13 -0400

    updated navigation with new components

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -3,27 +3,39 @@ import SearchBar from '../atoms/search-bar/SearchBar';
 import AppUtil from './app-util/AppUtil';
 import styles from './navigation.module.css';
 import ScrollablePanel from './scrollable-panel/ScrollablePanel';
+import { IconContext } from 'react-icons';
 
 function Navigation() {
   const [isHovering, setIsHovering] = useState(false);
 
-  const hoverStyle = { width: isHovering ? '331px' : '80px' };
+  const hoverStyle = {
+    width: isHovering ? '331px' : '80px',
+    alignItems: isHovering ? 'flex-start' : 'center',
+  };
+
+  const handleActivateSearchBar = () => {
+    setIsHovering(true);
+  };
 
   return (
-    <nav
-      className={styles.navigation}
-      style={hoverStyle}
-      onMouseEnter={() => setIsHovering(true)}
-      onMouseLeave={() => setIsHovering(false)}
-    >
-      <div>
-        <span>PEGA </span>
-        <span>Cosmos</span>
-      </div>
-      <SearchBar isHovering={isHovering} />
-      <ScrollablePanel />
-      <AppUtil />
-    </nav>
+    <IconContext.Provider value={{ size: '1.25rem', color: '#ffffffff' }}>
+      <nav
+        className={styles.navigation}
+        style={hoverStyle}
+        onMouseEnter={() => setIsHovering(true)}
+        onMouseLeave={() => setIsHovering(false)}
+      >
+        <div style={{ margin: '20px 20px' }} tabIndex={0}>
+          <span aria-label='company name'>PEGA</span>
+        </div>
+        <SearchBar
+          isHovering={isHovering}
+          onActivateSearchBar={handleActivateSearchBar}
+        />
+        <ScrollablePanel isHovering={isHovering} />
+        <AppUtil />
+      </nav>
+    </IconContext.Provider>
   );
 }
 
