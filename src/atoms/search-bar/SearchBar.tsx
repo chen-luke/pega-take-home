852e464d47ead9f244b182d16363edcc630175c0
chen-luke <72319988+chen-luke@users.noreply.github.com>
2025-10-25 09:42:06 -0400

    implemented search bar component

--- src/atoms/search-bar/SearchBar.tsx
+++ src/atoms/search-bar/SearchBar.tsx
@@ -1,14 +1,37 @@
 import styles from './search-bar.module.css';
 import { FaSearch } from 'react-icons/fa';
 
-function SearchBar({ isHovering }: { isHovering: boolean }) {
+function SearchBar({
+  isHovering,
+  onActivateSearchBar,
+}: {
+  isHovering: boolean;
+  onActivateSearchBar: () => void;
+}) {
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === 'Enter' || event.key === ' ') {
+      event.preventDefault();
+      onActivateSearchBar();
+    }
+  };
   return isHovering ? (
     <div className={styles.searchContainer}>
       <FaSearch className={styles.searchIcon} />
-      <input placeholder='search' className={styles.searchBar} />
+      <input
+        placeholder='search'
+        className={styles.searchBar}
+        tabIndex={0}
+        aria-label='Search input field'
+      />
     </div>
   ) : (
-    <div className={styles.containerNoHover}>
+    <div
+      className={styles.containerNoHover}
+      onClick={onActivateSearchBar}
+      onKeyDown={handleKeyDown}
+      tabIndex={0}
+      aria-label='Expand search bar'
+    >
       <FaSearch className={styles.searchIcon} />
     </div>
   );
