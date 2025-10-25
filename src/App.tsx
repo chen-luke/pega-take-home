300c86aba2a9752f58851d7a64771872b9121f6e
chen-luke <72319988+chen-luke@users.noreply.github.com>
2025-10-25 09:40:36 -0400

    added routing placeholder using react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navigation from './navigation/Navigation';
+import Epics from './epics/Epics';
+import Bugs from './bugs/Bugs';
 
 function App() {
-  return <Navigation />;
+  return (
+    <BrowserRouter>
+      <div style={{ display: 'flex' }}>
+        <Navigation />
+        <Routes>
+          <Route path='/epics' element={<Epics />} />
+          <Route path='/bugs' element={<Bugs />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
